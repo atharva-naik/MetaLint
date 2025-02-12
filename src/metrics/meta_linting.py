69c5cd9c9230c11d39c11d75c9ad4608628287fa
Atharva Naik <atharvanaik2018@gmail.com>
2025-02-12 17:31:26 -0500

    working coarse P,R,F metrics

--- src/metrics/meta_linting.py
+++ src/metrics/meta_linting.py
@@ -61,11 +61,47 @@ def compute_coarse_overlap_per_idiom(data):
             )
         ])/len(ground_truths)
 
-    return idiom_overlaps
+    coarse_precision = {k: 0 for k in idiom_codes}
+    coarse_recall = {k: 0 for k in idiom_codes}
+    for idiom_code in per_idiom_gts:
+        num = 0 # true positives.
+        denom_P = 0 # true positives + false positives.
+        denom_R = 0 # true positives + false negatives.
+        for index,count in enumerate(per_idiom_gts[idiom_code]):
+            if count > 0: 
+                denom_R += 1
+                if count == per_idiom_preds[idiom_code][index]: 
+                    num += 1
+        for count in per_idiom_preds[idiom_code]:
+            if count > 0: denom_P += 1
+        assert denom_P >= num
+        assert denom_R >= num    
+        coarse_precision[idiom_code] = num/denom_P if denom_P != 0 else 1
+        coarse_recall[idiom_code] = num/denom_R if denom_R != 0 else 1
+
+    return idiom_overlaps, coarse_precision, coarse_recall
 
 # main
 if __name__ == "__main__":
     model_preds = read_jsonl("data/meta_linting_preds/qwen2.5coder_3b_instruct_sft_preds.jsonl")
-    coarse_idiom_overlaps = compute_coarse_overlap_per_idiom(model_preds)
-    print(np.mean(list(coarse_idiom_overlaps.values())))
-    print(coarse_idiom_overlaps)
+    coarse_idiom_overlaps, coarse_P, coarse_R = compute_coarse_overlap_per_idiom(model_preds)
+    coarse_F = {}
+    
+    for idiom_code in coarse_P:
+        p = coarse_P[idiom_code]
+        r = coarse_R[idiom_code]
+        if p!=0 and r!=0:
+            f = 2*p*r/(p+r)
+        else: f = 0
+        coarse_F[idiom_code] = f
+
+    # print(coarse_idiom_overlaps)
+    # print(np.mean(list(coarse_idiom_overlaps.values())))
+    # print()
+    print(coarse_P)
+    print("P:", np.mean(list(coarse_P.values())))
+    print(coarse_R)
+    print("R:", np.mean(list(coarse_R.values())))
+    print(coarse_F)
+    print("F:", np.mean(list(coarse_F.values())))
+    # print()
